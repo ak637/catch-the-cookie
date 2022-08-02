8d36b0a14b3a90046770e41e72aae51a11972e50
ak637 <alexkittredge@gmail.com>
2022-08-03 00:53:18 +0100

    Added start page cookie background
    - cookies move

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>Catch the Cookie!</h1>
+      <h1 className="App__title">Catch the Cookie!</h1>
+      <div className="App__snow"></div>
     </div>
   );
 }
