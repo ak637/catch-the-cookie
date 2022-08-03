07353e129292f3c20701e03d67b130e03b112b2c
ak637 <alexkittredge@gmail.com>
2022-08-03 17:54:25 +0100

    Cookie Catch game appears when play is pressed
    - requires styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.scss";
 import Start from "./components/Start/Start";
+import { useState } from "react";
 
 function App() {
+  const [activeGame, setActiveGame] = useState("start");
+
   return (
-    <div className="App">
-      <div className="App__snow"></div>
-      <Start />
-    </div>
+    <>
+      {activeGame === "start" && (
+        <div className="App">
+          <div className="App__snow"></div>
+          <Start activeGame={activeGame} setActiveGame={setActiveGame} />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -4,38 +4,44 @@ import startAudio from "../../assets/audio/start.mp3";
 import CookieCatch from "../CookieCatch/CookieCatch";
 import { render } from "@testing-library/react";
 
-const Start = () => {
+const Start = (props) => {
+  console.log(props);
   const [audio] = useState(new Audio(startAudio));
 
-  const [activeGame, setActiveGame] = useState("start");
   const clickHandler = () => {
-    if (activeGame !== "CookieCatch") {
+    if (props.activeGame !== "CookieCatch") {
       render(<CookieCatch />);
-      setActiveGame("CookieCatch");
+      props.setActiveGame("CookieCatch");
+      console.log(props.activeGame);
+      audio.pause();
     }
   };
 
   return (
-    <section className="startContainer">
-      <h1 className="startContainer__title">Catch the Cookie!</h1>
-      <h3
-        className="startContainer__play"
-        onClick={clickHandler}
-        onMouseEnter={() => {
-          audio.play();
-        }}
-        onMouseLeave={() => {
-          audio.pause();
-          audio.currentTime = 0;
-        }}
-      >
-        PLAY!
-      </h3>
-      <div className="startContainer__subtitles">
-        <p className="startContainer__subtitles-text">How to play?</p>
-        <p className="startContainer__subtitles-text">High Scores</p>
-      </div>
-    </section>
+    <>
+      {props.activeGame === "start" && (
+        <section className="startContainer">
+          <h1 className="startContainer__title">Catch the Cookie!</h1>
+          <h3
+            className="startContainer__play"
+            onClick={clickHandler}
+            onMouseEnter={() => {
+              audio.play();
+            }}
+            onMouseLeave={() => {
+              audio.pause();
+              audio.currentTime = 0;
+            }}
+          >
+            PLAY!
+          </h3>
+          <div className="startContainer__subtitles">
+            <p className="startContainer__subtitles-text">How to play?</p>
+            <p className="startContainer__subtitles-text">High Scores</p>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
